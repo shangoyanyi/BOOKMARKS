596ea72a6df995e3bd5dc7bade459f53c6163ff2
chih-kuang,wu <shangoyanyi@gmail.com>
2016-05-13 14:25:48 +0800

    remove unused codes

--- app/js/popup.js
+++ app/js/popup.js
@@ -50,20 +50,7 @@ $(function(){
       }
   });
   
-  /*
-  $('#bookmark-container').append(newItemGroup("專案管理"));
-  $('#bookmark-container').append(newItem("Slack - chtmember", "https://chtmember.slack.com/", chrome.extension.getURL("app/img/slack200x200.png")));
-  $('#bookmark-container').append(newItem("Trello - 會員小隊", "https://trello.com/user75548250", chrome.extension.getURL("app/img/trello200x200.png")));
-  $('#bookmark-container').append(newItem("Quip", "https://quip.com", chrome.extension.getURL("app/img/quip200x200.png")));
   
-  $('#bookmark-container').append(spacing(30));
-  $('#bookmark-container').append(newItemGroup("公司網站"));
-  $('#bookmark-container').append(newItem("EIP | 員工資訊入口網站", "http://eip.cht.com.tw", chrome.extension.getURL("app/img/cht200x200.png")));
-  $('#bookmark-container').append(newItem("HRIS | 人力資源管理系統", "https://hris.cht.com.tw/", chrome.extension.getURL("app/img/cht200x200.png")));
-  $('#bookmark-container').append(newItem("eForm | 電子表單", "http://eform.cht.com.tw/Pages/Announcement/Announcement.aspx", chrome.extension.getURL("app/img/cht200x200.png")));
-  $('#bookmark-container').append(newItem("eMeeting | 電子會議", "http://emt.cht.com.tw/pub/docProcess.asp", chrome.extension.getURL("app/img/cht200x200.png")));
-  $('#bookmark-container').append(newItem("ODAS | 電子公文", "http://odaswps.cht.com.tw/Portal/Pages/", chrome.extension.getURL("app/img/cht200x200.png")));
-  */
   
   $('.bookmark-container').on('click', '.bookmark', function(){
     window.open($(this).data('target'));
